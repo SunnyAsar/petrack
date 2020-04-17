9d01d6893fc5b8750a3668c19f52173c5fde85bd
Sunny Asar <ring2rings93@gmail.com>
2020-04-17 18:29:20 +0100

    add jbuilder to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_161331) do
+ActiveRecord::Schema.define(version: 2020_04_17_172036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2020_04_17_161331) do
     t.uuid "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "amount", default: "0.0"
+    t.integer "quantity", default: 1
     t.index ["category_id"], name: "index_pets_on_category_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
